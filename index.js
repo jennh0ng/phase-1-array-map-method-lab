c160ae4589ce138d336fb38a03d58e902ff18b65
Jennifer Hong <hongjenn24@gmail.com>
2021-11-26 18:06:56 -0800

    Comleted assignment

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = tutorials.map(sentence => {
+//   sentence.charAt(0).toUpperCase() + sentence.slice(1); 
+// });
+
+// titleCased();
+
+// function titleCased() {
+//   const titleCased = tutorials.map(sentence => { console.log(sentence);
+//     sentence.charAt(0).toUpperCase() + sentence.slice(1); 
+//   });
+// }
+// titleCased();
+
+const titleCased = (input) => {
+  return tutorials.map( line => {
+    let letters = line.split(' ')
+    let capitalizedTokens =
+      letters.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+    let response = capitalizedTokens.join(' ')
+    return response
+  });
 }
